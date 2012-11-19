95a14d445ec42d6e27831a92566057443b8f9146
Jon Williams <djangofan@gmail.com>
2012-11-18 20:00:08 -0800

    Initial commit.

---
+++ src/main/java/qa/webdriver/GoogleSearchPage.java
@@ -0,0 +1,70 @@
+package qa.webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.SlowLoadableComponent;
+import org.openqa.selenium.support.ui.SystemClock;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static qa.webdriver.ExtendedUtilities.*;
+
+public class GoogleSearchPage extends SlowLoadableComponent<GoogleSearchPage> {
+	
+	private static int timeOutInSeconds = 3;
+
+    public GoogleSearchPage() {
+        super( new SystemClock(), timeOutInSeconds);
+		System.out.println("Loaded Google Search Page");
+		this.get(); //calls load and isLoaded
+		PageFactory.initElements(driver, this); 
+    }
+
+	@Override
+	protected void load() {
+		System.out.println("Calling GoogleSearchPage.load...");
+		Wait<WebDriver> wait = new WebDriverWait(driver, 30); 
+		if ( driver.findElement( By.id("//div[@id='navBar']//div[1]")).getAttribute("onclick") == null )
+		{			
+			WebElement defnav = wait.until( visibilityOfElementLocated( By.id("gbqfq") ) );
+			defnav.click();
+			WebElement adnav = wait.until( visibilityOfElementLocated( By.id("gbqfq") ) );
+			adnav.click();			
+		} else {
+			WebElement adnav = wait.until( visibilityOfElementLocated( By.id("gbqfq") ) );
+			adnav.click();
+		}
+	}
+
+	@Override
+	protected void isLoaded() throws Error {
+		System.out.println("Calling GoogleSearchPage.isLoaded...");
+		final WebElement coreField = (new WebDriverWait(driver, 10))
+				.until(new ExpectedCondition<WebElement>(){
+					public WebElement apply(WebDriver d) {
+						return d.findElement( By.id("gbqfq") );
+					}});
+		if ( coreField.isDisplayed() )
+		{
+			System.out.println("Google search page is loaded.\nWill initialize page object.");
+		} else {
+			System.out.println("Google search page is not yet loaded." );
+		} 
+	}
+	
+	@FindBy(id = "gbqfq") private WebElement searchField;
+	public void setSearchString( String sstr ) {
+		clearAndType( searchField, sstr );
+	}
+	
+	@FindBy(id = "gbqfb") private WebElement searchButton;
+	public void clickSearchButton() {
+		searchButton.click();
+	}
+	
+    
+}
---
+++ src/main/java/qa/webdriver/UtilityClass.java
@@ -0,0 +1,113 @@
+package qa.webdriver;
+
+import java.io.File;
+import java.text.DecimalFormat;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Point;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.Keys;
+
+public class UtilityClass {
+
+	static WebDriver driver;
+	private static JavascriptExecutor js;
+	static String pageLoadStatus = null;
+	
+	protected UtilityClass() {
+		throw new AssertionError(); // to prevent instantiation as an object
+	}
+	
+	public static void clearAndSetValue(WebElement field, String text) { 
+		field.clear(); 
+		field.sendKeys(Keys.chord(Keys.CONTROL, "a"), text); 
+	}
+
+	public static void clearAndType(WebElement field, String text) { 
+		field.clear(); 
+		field.sendKeys(text); 
+	}
+	
+	public static File loadTestFile( String fileName ) {
+		File gradleFile = new File( fileName );
+		File junitFile = new File("src/test/resources/qa/webdriver/" + fileName );
+		if ( gradleFile.exists() ) {
+			System.out.println("Loaded '" + gradleFile.getAbsolutePath() + "' from Gradle runtime.");
+			return gradleFile;
+		} else if ( junitFile.exists() ) {
+			System.out.println("Loaded '" + junitFile.getAbsolutePath() + "' from JUnit runtime.");
+			return junitFile;
+		}
+		System.out.println("Problem loading test data input file.  Trying to run from Gradle bin directory...");
+		return new File("bin/qa/webdriver" + fileName);
+	}
+	
+	public static ExpectedCondition<WebElement> visibilityOfElementLocated(final By locator) {
+		return new ExpectedCondition<WebElement>() {
+			public WebElement apply(WebDriver driver) {
+				WebElement toReturn = driver.findElement(locator);
+				if (toReturn.isDisplayed()) {
+					return toReturn;
+				}
+				return null;
+			}
+		};
+	}
+	
+	public static void initializeBrowser( String type ) {
+		if ( type.equalsIgnoreCase( "firefox" ) ) {
+			driver = new FirefoxDriver();
+		} else if ( type.equalsIgnoreCase( "ie" ) ) {
+			driver = new InternetExplorerDriver();
+		}
+        driver.manage().timeouts().implicitlyWait( 10000, TimeUnit.MILLISECONDS );
+        driver.manage().window().setPosition(new Point(200, 10));
+        driver.manage().window().setSize(new Dimension(1200, 800));
+	}
+
+	public static void clickElementWithJSE( String id ) {
+		//Create the object of JavaScript Executor 
+		//click command through Javascript
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		WebElement element= driver.findElement( By.id( id ) );
+		//Use any locator type using to identify the element
+		js.executeScript( "arguments[0].click();", element );
+		js = null;
+	}
+
+	public static void waitForPageToLoad() {
+		System.out.println("Waiting for page to load...");
+		do {
+			js = (JavascriptExecutor) driver;
+			pageLoadStatus = (String)js.executeScript("return document.readyState");
+			System.out.print(".");
+		} while ( !pageLoadStatus.equals("complete") );
+		System.out.println();
+	}
+
+	public static void waitTimer( int units, int mills ) {
+		DecimalFormat df = new DecimalFormat("###.##");
+		double totalSeconds = ((double)units*mills)/1000;
+		System.out.print("Explicit pause for " + df.format(totalSeconds) + " seconds divided by " + units + " units of time: ");
+		try {
+			Thread.currentThread();		
+			int x = 0;
+			while( x < units ) {
+				Thread.sleep( mills );
+				System.out.print("." );
+				x = x + 1;
+			}
+			System.out.print('\n');
+		} catch ( InterruptedException ex ) {
+			ex.printStackTrace();
+		}	
+	}
+
+}
