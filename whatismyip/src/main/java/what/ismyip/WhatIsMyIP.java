c4089bc7126b77a7d04f87523b10078d2d53995d
Jon Williams <djangofan@gmail.com>
2013-02-05 21:03:22 -0800

    Added a WhatIsMyIP project.

---
+++ whatismyip/src/main/java/what/ismyip/WhatIsMyIP.java
@@ -0,0 +1,38 @@
+package what.ismyip;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class WhatIsMyIP 
+{
+    public static void main( String[] args )
+    {
+    	String msg = System.getProperty("simple.message") + "\n";
+    	msg = msg + "My IP Address Is" + "\n";
+        HtmlUnitDriver driver = new HtmlUnitDriver(true);
+        driver.get("http://checkip.dyndns.com/");        
+        String ipstring = driver.findElement( By.cssSelector("html body") ).getText();        
+        msg = msg + ipstring.substring( ipstring.indexOf(": ") + 2) + "\n";
+        
+        System.out.println( msg );
+        FileWriter outFile = null;
+		try {
+			outFile = new FileWriter("ip.txt");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        PrintWriter out = new PrintWriter(outFile);
+        out.write(msg);
+        out.close();
+        try {
+			outFile.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        
+    }
+}
