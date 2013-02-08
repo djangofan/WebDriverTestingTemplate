f47c0bc0f3d52fe51cb9c19c6eee47ea8532708b
Jon Williams <djangofan@gmail.com>
2013-02-08 11:33:12 -0800

    Added comment to startWebDriverServer.bat

--- startWebDriverServer.bat
+++ startWebDriverServer.bat
@@ -1,5 +1,16 @@
 @ECHO off
-:: start server this way until i figure out how to get my WebDriverServer.java class to work correctly
+SETLOCAL ENABLEDELAYEDEXPANSION
+ECHO.
+ECHO The projects in this template are written to instantiate the ^"WebDriver
+ECHO JSON Hub Server^" as part of the unit tests.  If you really intend to
+ECHO continue and start this server manually, make sure you understand the 
+ECHO implications.
+ECHO.
+ECHO Click any key to continue and start the server...
+ECHO.
+pause>nul
+timeout 10
+
 java -jar selenium-server-standalone-2.28.0.jar
 
 pause
