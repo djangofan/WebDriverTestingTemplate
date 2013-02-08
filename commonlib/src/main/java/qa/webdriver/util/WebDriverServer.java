ff9dff44f5474e5c6ee9258efdae4853de9a48ca
Jon Williams <djangofan@gmail.com>
2013-02-08 08:09:25 -0800

    Added web driver server class.
    So that we don't need to run the server separately using the .jar
    executor.

--- commonlib/src/main/java/qa/webdriver/util/WebDriverServer.java
+++ commonlib/src/main/java/qa/webdriver/util/WebDriverServer.java
@@ -1,42 +1,104 @@
 package qa.webdriver.util;
 
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.nio.SelectChannelConnector;
-import org.mortbay.jetty.webapp.WebAppContext;
-import java.io.File;
-import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.openqa.selenium.remote.server.DriverServlet;
+import org.openqa.grid.common.GridRole;
+import org.openqa.grid.common.RegistrationRequest;
+import org.openqa.grid.common.SeleniumProtocol;
+import org.openqa.grid.internal.utils.GridHubConfiguration;
+import org.openqa.grid.internal.utils.SelfRegisteringRemote;
+import org.openqa.grid.web.Hub;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class WebDriverServer {
 	
-	// this is all a mess.  i cant figure out how to get this to work
-	
-  private Server server = new Server();
+	String hubHost = "localhost";
+	int hubPort = 4444;
+	Hub myHub = null;
+	SelfRegisteringRemote remoteWebDriverNode = null, remoteRCNode = null;
 
-  public WebDriverServer() {
-	  
-    WebAppContext context = new WebAppContext();
-    context.setContextPath("");    
-    context.setWar( new File(".").getAbsolutePath() );
-    server.setHandler( context );
-    try {
-		WebAppContext.addWebApplications(server, "org.mortbay.jetty.servlet.DefaultServlet", "/", true, true);
-	} catch (IOException e) {
-		e.printStackTrace();
+	public WebDriverServer () {
+		bringUpHubAndNode();
+		
 	}
-    //context.addServlet( DriverServlet.class, "/wd/*");
-    //SelectChannelConnector connector = new SelectChannelConnector();
-    //connector.setPort(4444);
-    //server.(connector);
-    
-    try {
-		server.start();
-	} catch (Exception e) {
-		e.printStackTrace();
-	}  //requires servlet-api 3+ ???
-  
+	
+	public static void main(String[] args) {
 
-  }
-  
+		new WebDriverServer();
+		
+	}
+	
+	public void bringUpHubAndNode() {
+		GridHubConfiguration gridHubConfig = new GridHubConfiguration();
+		gridHubConfig.setHost(hubHost);
+		gridHubConfig.setPort(hubPort);
+		myHub = new Hub(gridHubConfig);
+		try {
+			myHub.start();
+		} catch (Exception e) {
+			System.out.println("Error starting hub.");
+			e.printStackTrace();
+		}
+		DesiredCapabilities chrome = DesiredCapabilities.chrome();
+		chrome.setBrowserName("*googlechrome");
+		try {
+			remoteRCNode = attachNodeToHub(chrome, GridRole.NODE, 5555,
+					SeleniumProtocol.Selenium);
+			remoteWebDriverNode = attachNodeToHub(DesiredCapabilities.firefox(),
+					GridRole.NODE, 5556, SeleniumProtocol.WebDriver);
+		} catch (Exception e) {
+			System.out.println("Error attaching node.");
+			e.printStackTrace();
+		}		
+	}
+
+	private SelfRegisteringRemote attachNodeToHub( DesiredCapabilities capability, GridRole role,
+			               int nodePort, SeleniumProtocol protocol) throws Exception {
+		SelfRegisteringRemote node = null;
+		RegistrationRequest registrationRequest = new RegistrationRequest();
+		capability.setCapability("seleniumProtocol", protocol);
+		registrationRequest.addDesiredCapability(capability);
+		registrationRequest.setRole(role);
+		registrationRequest.setConfiguration(fetchNodeConfiguration(role, nodePort, protocol));
+		node = new SelfRegisteringRemote(registrationRequest);
+		node.startRemoteServer();
+		node.startRegistrationProcess();
+		return node;
+	}
+
+	private Map<String, Object> fetchNodeConfiguration(GridRole role,
+			int portToRun, SeleniumProtocol protocol)
+			throws MalformedURLException {
+		Map<String, Object> nodeConfiguration = new HashMap<String, Object>();
+		nodeConfiguration.put(RegistrationRequest.AUTO_REGISTER, true);
+		nodeConfiguration.put(RegistrationRequest.HUB_HOST, myHub.getHost());
+		nodeConfiguration.put(RegistrationRequest.HUB_PORT, myHub.getPort());
+		nodeConfiguration.put(RegistrationRequest.PORT, portToRun);
+		URL remoteURL = new URL("http://" + myHub.getHost() + ":" + portToRun);
+		nodeConfiguration.put(RegistrationRequest.PROXY_CLASS,
+				"org.openqa.grid.selenium.proxy.DefaultRemoteProxy");
+		nodeConfiguration.put(RegistrationRequest.MAX_SESSION, 1);
+		nodeConfiguration.put(RegistrationRequest.CLEAN_UP_CYCLE, 2000);
+		nodeConfiguration.put(RegistrationRequest.REMOTE_HOST, remoteURL);
+		nodeConfiguration.put(RegistrationRequest.MAX_INSTANCES, 1);
+		return nodeConfiguration;
+	}
+
+	public void shutDownNodeAndHub() throws Exception {
+		if (remoteWebDriverNode != null) {
+			remoteWebDriverNode.stopRemoteServer();
+			System.out.println("WebDriver Node shutdown");
+		}
+		if (remoteRCNode != null) {
+			remoteRCNode.stopRemoteServer();
+			System.out.println("RC Node shutdown");
+		}
+		if ( myHub != null ) {
+			myHub.stop();
+			System.out.println("Local hub shutdown");
+		}
+	}
 }
