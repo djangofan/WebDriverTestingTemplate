6b5dfd8da3810fbfcd6996e177b1d76a88d1c975
Jon Williams <djangofan@gmail.com>
2013-03-13 12:11:00 -0700

    Update to startWeb.bat for parallelwebdriver .

--- parallelwebdriver/startWeb.bat
+++ parallelwebdriver/startWeb.bat
@@ -10,13 +10,21 @@ IF %ERRORLEVEL%==1 (
   GOTO :END
 )
 
-SET "BUILD_HOME=%~dp0"
-SET HTTPLIB=lib\httpcore-4.2.2.jar
-SET "HTTPROOT=%BUILD_HOME%build\resources\test"
+SET "SCRIPT_HOME=%~dp0"
+SET "BUILD_HOME=%SCRIPT_HOME%build"
+SET HTTPLIB=httpcore-4.2.2.jar
+SET "HTTPROOT=%BUILD_HOME%\resources\test"
 
-IF NOT EXIST "%HTTPLIB%" (
-  ECHO The httpcore library may be missing.  Required to start web server.
-  GOTO :END
+IF NOT EXIST "%BUILD_HOME%\classes\main\webdriver\test\GradleHttpServer.class" (
+  ECHO You need to compile GradleHttpServer.java before this will run.
+  ECHO Expecting it at: ^"%BUILD_HOME%\classes\main\webdriver\test\GradleHttpServer.class^"
+  GOTO :ERROR
+)
+
+IF NOT EXIST "%SCRIPT_HOME%lib\%HTTPLIB%" (
+  ECHO The httpcore library may be missing from ^"%SCRIPT_HOME%lib\%HTTPLIB%^".
+  ECHO Required to start web server.
+  GOTO :ERROR
 )
 
 IF EXIST "%HTTPROOT%" (
@@ -24,16 +32,19 @@ IF EXIST "%HTTPROOT%" (
   CD "%HTTPROOT%"
 ) ELSE (
   ECHO The directory ^"%HTTPROOT%^" does not exist.
-  GOTO :END
+  GOTO :ERROR
 )
 
-SET "CLASSPATH=.;%BUILD_HOME%%HTTPLIB%;%BUILD_HOME%build\classes\main"
+SET "CLASSPATH=.;%SCRIPT_HOME%lib\%HTTPLIB%;%BUILD_HOME%\classes\main"
 ECHO CLASSPATH=%CLASSPATH%
 ECHO.
 ECHO Starting web server...
 
 java.exe -cp %CLASSPATH% webdriver.test.GradleHttpServer "%HTTPROOT%"
 
+GOTO :END
+
+:ERROR
+pause
 :END
 ECHO End of script.  Server stopped.
-::timeout 5
