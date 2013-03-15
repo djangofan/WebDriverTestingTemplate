748cd830f2e56d919da20456b53f1c9a38a89a20
Jon Thor Austen <djangofan@gmail.com>
2013-03-15 14:58:30 -0700

    Update startWebDriverGridNode.bat

--- root/startWebDriverGridNode.bat
+++ root/startWebDriverGridNode.bat
@@ -3,8 +3,7 @@ SETLOCAL ENABLEDELAYEDEXPANSION
 TITLE WebDriver Grid Node
 
 set CHROME_DRIVER_LOC=chromedriver.exe
-:: place IEDriverServer.exe in current path
-SET PATH=%CD%;%PATH%
+SET JAR=selenium-server-standalone-2.31.0.jar
 
 ECHO *********************************************
 ECHO *
@@ -13,13 +12,27 @@ ECHO * that a WebDriver JSON Hub is already running, usually on port 4444.
 ECHO *
 ECHO *********************************************
 ECHO.
+
+IF NOT DEFINED JAVA_HOME (
+  ECHO You must define a JAVA_HOME environment variable before you run this script.
+  GOTO :ERROR
+)
+SET "PATH=.;%JAVA_HOME%\bin;%PATH%"
+
+IF NOT EXIST %JAR% (
+  "C:\Program Files (x86)\GnuWin32\bin\wget.exe" --dot-style=binary http://selenium.googlecode.com/files/%JAR%
+)
+
 java.exe -version
+ECHO.
 ECHO.
 ECHO ======================
 ECHO Grid Hub status : & netstat -an | FIND "4444"
 ECHO ======================
 ECHO.
-java.exe -jar selenium-server-standalone-2.30.0.jar -role node -nodeConfig NodeConfig.json -Dwebdriver.chrome.driver=%CHROME_DRIVER_LOC%
+java.exe -jar %JAR% -role node -nodeConfig Node1Config.json -Dwebdriver.chrome.driver=%CHROME_DRIVER_LOC%
 
-::pause
-
+GOTO :END
+:ERROR
+pause
+:END
