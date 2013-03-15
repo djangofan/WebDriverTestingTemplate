0acc225d39b1e87b7b63c66ba68682a27b7b7766
Jon Thor Austen <djangofan@gmail.com>
2013-03-15 14:57:12 -0700

    Update startWebDriverGridHub.bat

--- root/startWebDriverGridHub.bat
+++ root/startWebDriverGridHub.bat
@@ -1,17 +1,34 @@
 @ECHO off
 SETLOCAL ENABLEDELAYEDEXPANSION
 TITLE WebDriver Grid Hub on 4444
+
 ECHO *********************************************
 ECHO *
 ECHO * WebDriver grid Hub instance.  
 ECHO *
 ECHO *********************************************
 ECHO.
+
+SET JAR=selenium-server-standalone-2.31.0.jar
+
+IF NOT DEFINED JAVA_HOME (
+  ECHO You must define a JAVA_HOME environment variable before you run this script.
+  GOTO :ERROR
+)
+SET "PATH=%JAVA_HOME%\bin;%PATH%"
+
+IF NOT EXIST %JAR% (
+  "C:\Program Files (x86)\GnuWin32\bin\wget.exe" --dot-style=binary http://selenium.googlecode.com/files/%JAR%
+)
+
 java.exe -version
 ECHO.
-java.exe -jar selenium-server-standalone-2.30.0.jar -role hub -hubConfig HubConfig.json -debug
 
-::java.exe -jar selenium-server-standalone-2.30.0.jar -role hub -browser "browserName=firefox,version=19,firefox_binary=C:\Program Files (x86)\Mozilla Firefox\firefox.exe ,maxInstances=5, platform=WINDOWS" -debug
+java.exe -jar %JAR% -role hub -hubConfig HubConfig.json -debug
 
-::pause
+::java.exe -jar %JAR% -role hub -browser "browserName=firefox,version=19,firefox_binary=C:\Program Files (x86)\Mozilla Firefox\firefox.exe ,maxInstances=5, platform=WINDOWS" -debug
 
+GOTO :END
+:ERROR
+pause
+:END
