b821b7686ffebe7eec3ccec344495da940365c56
Jon Austen <djangofan@gmail.com>
2013-03-15 19:13:04 -0700

    Fixes for Grid and Node start script.

--- root/startWebDriverGridHub.bat
+++ root/startWebDriverGridHub.bat
@@ -1,33 +1,36 @@
 @ECHO off
 SETLOCAL ENABLEDELAYEDEXPANSION
 TITLE WebDriver Grid Hub on 4444
-
+ 
 ECHO *********************************************
 ECHO *
-ECHO * WebDriver grid Hub instance.  
+ECHO * WebDriver grid Hub instance.
 ECHO *
 ECHO *********************************************
 ECHO.
-
+ 
 SET JAR=selenium-server-standalone-2.31.0.jar
-
+SET "WGET=C:\Program Files (x86)\GnuWin32\bin\wget.exe"
+ 
 IF NOT DEFINED JAVA_HOME (
-  ECHO You must define a JAVA_HOME environment variable before you run this script.
-  GOTO :ERROR
+ECHO You must define a JAVA_HOME environment variable before you run this script.
+GOTO :ERROR
 )
 SET "PATH=%JAVA_HOME%\bin;%PATH%"
-
+ 
 IF NOT EXIST %JAR% (
-  "C:\Program Files (x86)\GnuWin32\bin\wget.exe" --dot-style=binary http://selenium.googlecode.com/files/%JAR%
+IF EXIST "%WGET%" (
+"%WGET%" --dot-style=binary http://selenium.googlecode.com/files/%JAR%
+) ELSE (
+ECHO Wget.exe is missing. Install GNU Utils. & GOTO :ERROR
 )
-
-java.exe -version
+)
 ECHO.
-
-java.exe -jar %JAR% -role hub -hubConfig HubConfig.json -debug
-
+ 
+java.exe -jar %JAR% -role hub -hubConfig hubConfig.json -debug
+ 
 ::java.exe -jar %JAR% -role hub -browser "browserName=firefox,version=19,firefox_binary=C:\Program Files (x86)\Mozilla Firefox\firefox.exe ,maxInstances=5, platform=WINDOWS" -debug
-
+ 
 GOTO :END
 :ERROR
 pause
--- root/startWebDriverGridNode.bat
+++ root/startWebDriverGridNode.bat
@@ -2,47 +2,66 @@
 SETLOCAL ENABLEDELAYEDEXPANSION
 SET TITLETEXT=WebDriver Grid Node
 TITLE %TITLETEXT%
-
-set CHROME_DRIVER_LOC=chromedriver.exe
+ 
+SET CHROMEDRIVERZIP=chromedriver_win_26.0.1383.0.zip
+SET CHROMEDRIVER=chromedriver.exe
 SET JAR=selenium-server-standalone-2.31.0.jar
 SET IEDRIVERZIP=IEDriverServer_Win32_2.31.0.zip
 SET IEDRIVER=IEDriverServer.exe
-
+SET "WGET=C:\Program Files (x86)\GnuWin32\bin\wget.exe"
+ 
 ECHO *********************************************
 ECHO *
-ECHO * This script will start a WebDriver grid Node instance.  It requires
-ECHO * that a WebDriver JSON Hub is already running, usually on port 4444.
+ECHO * WebDriver Grid Node
+ECHO * It requires that a WebDriver JSON Hub is already running, usually on port 4444.
+ECHO * You can run more than one of these if each has its own JSON config file.
 ECHO *
 ECHO *********************************************
 ECHO.
-
+ 
 IF NOT DEFINED JAVA_HOME (
-  ECHO You must define a JAVA_HOME environment variable before you run this script.
-  GOTO :ERROR
+ECHO You must define a JAVA_HOME environment variable before you run this script.
+GOTO :ERROR
 )
 SET "PATH=.;%JAVA_HOME%\bin;%PATH%"
-
+ 
 IF NOT EXIST %JAR% (
-  "C:\Program Files (x86)\GnuWin32\bin\wget.exe" --dot-style=binary http://selenium.googlecode.com/files/%JAR%
+IF EXIST "%WGET%" (
+"%WGET%" --dot-style=binary http://selenium.googlecode.com/files/%JAR%
+) ELSE (
+ECHO Wget.exe is missing. Install GNU utils. & GOTO :ERROR
 )
-
+)
+ 
 IF NOT EXIST %IEDRIVER% (
-  "C:\Program Files (x86)\GnuWin32\bin\wget.exe" --dot-style=binary http://selenium.googlecode.com/files/%IEDRIVERZIP%
-  jar.exe xvf %IEDRIVERZIP%
-  DEL /Q %IEDRIVERZIP%
+IF EXIST "%WGET%" (
+"%WGET%" --dot-style=binary http://selenium.googlecode.com/files/%IEDRIVERZIP%
+jar.exe xvf %IEDRIVERZIP%
+DEL /Q %IEDRIVERZIP%
+) ELSE (
+ECHO Wget.exe is missing. & GOTO :ERROR
 )
-
-java.exe -version
-ECHO.
+)
+ 
+IF NOT EXIST %CHROMEDRIVER% (
+IF EXIST "%WGET%" (
+"%WGET%" --dot-style=binary --no-check-certificate https://chromedriver.googlecode.com/files/%CHROMEDRIVERZIP%
+jar.exe xvf %CHROMEDRIVERZIP%
+DEL /Q %CHROMEDRIVERZIP%
+) ELSE (
+ECHO Wget.exe is missing. & GOTO :ERROR
+)
+)
+ 
 ECHO.
 ECHO ======================
 ECHO Grid Hub status : & netstat -an | FIND "4444"
 ECHO ======================
 ECHO.
-
+ 
 TITLE %TITLETEXT%
-java.exe -jar %JAR% -role node -nodeConfig Node1Config.json -Dwebdriver.chrome.driver=%CHROME_DRIVER_LOC%
-
+java.exe -jar %JAR% -role node -nodeConfig node1Config.json -Dwebdriver.chrome.driver=%CHROMEDRIVER%
+ 
 GOTO :END
 :ERROR
 pause
